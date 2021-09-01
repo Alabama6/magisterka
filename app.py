8d4945ed0719621886bf56eff9b5e0401db86bd6
matylda <matylda.repa@gmail.com>
2021-09-01 20:52:38 +0200

    basic app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Amanda Stan',
+        'title': 'Orange new Black',
+    },
+    {
+        'author': 'Jane Carey',
+        'title': 'Summer in the City',
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
