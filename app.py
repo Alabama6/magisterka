f388d0162290ce98a6ddd159c96d2385270f1ddd
matylda <matylda.repa@gmail.com>
2021-09-08 21:34:53 +0200

    add login and register tab

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f6707d781deb2a53d03abf27b09c3c52'
 
 posts = [
     {
@@ -13,15 +16,37 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if True:
+            flash(f"You have been logged in!", "success")
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+        return redirect(url_for("home"))
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == '__main__':
